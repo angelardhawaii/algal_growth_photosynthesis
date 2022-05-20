e8dd7896e458d1c1090039ce4f32182312c2fdd6
angelardhawaii <angelard@hawaii.edu>
2022-05-19 14:16:09 -1000

    Several improvements to many of the files
    Added markdown files, modified the ps script by adding the growth rate to the individual ulva and hypnea data frames so I can run regressions with ps vs growth.
    
    Added some ggplots to the markdown that may or may not work at this point. They work in the .R file.

--- growth_rate_script.R
+++ growth_rate_script.R
@@ -9,41 +9,48 @@ library(emmeans)
 library(DHARMa)
 library(performance)
 library(patchwork)
+library(ggplot2)
 
 #open weight dataset and make columns for growth rate from initial and final weights
-run5_growth <- read.csv("/Users/Angela/Library/Mobile Documents/com~apple~CloudDocs/research_limu/run5_hyp_ulv_temp/final_temperature_run1-4/final_data_files/run5_growth_all_032422.csv")
+run5.6_growth <- read.csv("/Users/Angela/src/work/limu/algal_growth_photosynthesis/data/run5-6_growth_all_042922.csv")
 
 #make a new column for weight change (difference final from initial)
-run5_growth$growth_rate_percent <- (run5_growth$final.weight - run5_growth$Inital.weight) / run5_growth$Inital.weight * 100
+run5.6_growth$growth_rate_percent <- (run5.6_growth$final.weight - run5.6_growth$Initial.weight) / run5.6_growth$Initial.weight * 100
 
 #make a new column for daily growth rate from 8 day study (steady growth rate assumed rather than exponential)
-run5_growth$steady_growth_daily <- run5_growth$growth_rate_percent / 8
+run5.6_growth$steady_growth_daily <- run5.6_growth$growth_rate_percent / 8
 
 #make a new column that keeps only the numerical values (effectively removes the C in temperatures for consistency)
-run5_growth$temp_clean <- as.factor(substr(run5_growth$temperature, 1, 2))
+run5.6_growth$temp_clean <- as.factor(substr(run5.6_growth$temperature, 1, 2))
 
 #assigns temperature as a factor
-run5_growth$temperature <- as.factor(run5_growth$temp_clean)
+run5.6_growth$temperature <- as.factor(run5.6_growth$temp_clean)
 
 #assigns treatment as characters from integers then to factors
-run5_growth$treatment <- as.factor(as.character(run5_growth$treatment))
+run5.6_growth$treatment <- as.factor(as.character(run5.6_growth$treatment))
 
 #assign run as a factor
-run5_growth$run <- as.factor(run5_growth$run)
+run5.6_growth$run <- as.factor(run5.6_growth$run)
+
+#assign plant ID as a factor
+run5.6_growth$plant.ID <- as.factor(run5.6_growth$plant.ID)
+
+#assign RLC order as a factor
+run5.6_growth$RLC.order <- as.factor(run5.6_growth$RLC.order)
 
 # assign lunar phase as factor
-run5_growth$lunar.phase <- as.factor(run5_growth$lunar.phase)
+run5.6_growth$lunar.phase <- as.factor(run5.6_growth$lunar.phase)
 
 #toggle between the species for output
-hypnea <- subset(run5_growth, Species == "Hm")
-ulva <- subset(run5_growth, Species == "Ul")
+hypnea <- subset(run5.6_growth, Species == "Hm")
+ulva <- subset(run5.6_growth, Species == "Ul")
 
 # run model with interaction between temperature and treatment 
-#run5_growth_model <- lmer(formula = growth_rate_percent ~ treatments * temp_clean + (1 | run), data = hypnea)
+#run5.6_growth_model <- lmer(formula = growth_rate_percent ~ treatments * temp_clean + (1 | run), data = hypnea)
 
 #ULVA 
 #run model without interaction (0 in model permits display of four levels of treatments - no intercept)
-run5_growth_model_noint <- lmer(formula = growth_rate_percent ~ treatment + temperature + (1 | run), data = ulva)
+run5.6_growth_model_noint <- lmer(formula = growth_rate_percent ~ treatment + temperature + (1 | run) + (1 | plant.ID) + (1 | RLC.order), data = ulva)
 #make a histogram of the data for ulva
 hist(ulva$growth_rate_percent, main = paste("Ulva lactuca Growth Rate (%)"), col = "olivedrab3", labels = TRUE)
 ulva_growth_model_aov <- aov(growth_rate_percent ~ treatment + temperature, data = ulva)
@@ -51,20 +58,20 @@ TukeyHSD(ulva_growth_model_aov, "treatment", ordered = FALSE)
 
 #HYPNEA 
 #run model without interaction  (0 in model permits display of four levels of treatments - no intercept)
-run5_growth_model_noint <- lmer(formula = growth_rate_percent ~ treatment + temperature + (1 | run), data = hypnea)
+run5.6_growth_model_noint <- lmer(formula = growth_rate_percent ~ treatment + temperature + (1 | run) + (1 | plant.ID) + (1 | RLC.order), data = hypnea)
 #OR make a histogram of the data for hypnea
 hist(hypnea$growth_rate_percent, main = paste("Hypnea musciformis Growth Rate (%)"), col = "maroon", labels = TRUE)
 hypnea_growth_model_aov <- aov(growth_rate_percent ~ treatment + temperature, data = hypnea)
 TukeyHSD(hypnea_growth_model_aov, "treatment", ordered = FALSE)
 
 #check the performance of the model for each dataset: ulva and hypnea
-performance::check_model(run5_growth_model_noint)
+performance::check_model(run5.6_growth_model_noint)
 
-r.squaredGLMM(run5_growth_model_noint)
-summary(run5_growth_model_noint)
+r.squaredGLMM(run5.6_growth_model_noint)
+summary(run5.6_growth_model_noint)
 
-plot(allEffects(run5_growth_model_noint))
-anova(run5_growth_model_noint, type = c("III"), ddf = "Satterthwaite")
-plot(resid(run5_growth_model_noint) ~ fitted(run5_growth_model_noint))
-qqnorm(resid(run5_growth_model_noint))
-qqline(resid(run5_growth_model_noint))
+plot(allEffects(run5.6_growth_model_noint))
+anova(run5.6_growth_model_noint, type = c("III"), ddf = "Satterthwaite")
+plot(resid(run5.6_growth_model_noint) ~ fitted(run5.6_growth_model_noint))
+qqnorm(resid(run5.6_growth_model_noint))
+qqline(resid(run5.6_growth_model_noint))
--- photosyn_model_script.R
+++ photosyn_model_script.R
@@ -10,34 +10,16 @@ library(emmeans)
 library(DHARMa)
 library(performance)
 library(patchwork)
+library(ggplot2)
+library(ggpubr)
 
-run5_6_photosyn_data <- read.csv("/Users/Angela/Library/Mobile Documents/com~apple~CloudDocs/research_limu/run5-6_hyp_ulv_temp/final_temperature_run1-4/final_data_files/run5_run6_all_photosyn.csv")
+run5_6_photosyn_data <- read.csv("data_input/run5-6_ek_alpha.csv")
 
-get_run_number <- function(d) {
-  # returns the run number based on the input date  
-  run = NA
-  if ("2021-09-20" <= d && d <= "2021-09-29") {
-    run <- 1
-  } else if ("2021-09-30" <= d && d <= "2021-10-09") {
-    run <- 2
-  } else if ("2021-10-11" <= d && d <= "2021-10-29") {
-    run <- 3
-  } else if ("2021-11-01" <= d && d <= "2021-11-12") {
-    run <- 4
-  } else if ("2022-02-11" <= d && d <= "2022-03-01") {
-    run <- 0
-  } else {
-    print("The date is %s not in a recognized range", d)
-  }
-  run
-}
+# assign run as a factor
+run5_6_photosyn_data$Run <- as.factor(run5_6_photosyn_data$Run)
 
-# make new column adding run to the dataset
-run5_6_photosyn_data$run <- sapply(run5_6_photosyn_data$Date, get_run_number)
-run5_6_photosyn_data$run <- as.factor(run5_6_photosyn_data$run)
-
-#assigns temperature as a factor
-run5_6_photosyn_data$Temperature <- as.factor(run5_6_photosyn_data$Temperature)
+#assign temperature as a factor
+run5_6_photosyn_data$Temperature <- as.factor(run5_6_photosyn_data$Temp...C.)
 
 #assigns treatment as characters from integers then to factors
 run5_6_photosyn_data$Treatment <- as.factor(as.character(run5_6_photosyn_data$Treatment))
@@ -45,6 +27,14 @@ run5_6_photosyn_data$Treatment <- as.factor(as.character(run5_6_photosyn_data$Tr
 #toggle between the species for output. Use Day 9 for final analysis
 hypnea <- subset(run5_6_photosyn_data, Species == "hm" & RLC.Day == 9)
 ulva <- subset(run5_6_photosyn_data, Species == "ul" & RLC.Day == 9)
+
+#add growth rate from other dataset to this one and subset by species
+growth_rate <- read.csv("/Users/Angela/src/work/limu/algal_growth_photosynthesis/data_input/run5-6_growth_all_042922.csv")
+gr_ulva <- subset(growth_rate, Species == "Ul")
+gr_hypnea <- subset(growth_rate, Species == "Hm" & final.weight != 0.1017)
+
+ulva$growth_rate <- round((gr_ulva$final.weight - gr_ulva$Initial.weight) / gr_ulva$Initial.weight * 100, digits = 2)
+hypnea$growth_rate <- round((gr_hypnea$final.weight - gr_hypnea$Initial.weight) / gr_hypnea$Initial.weight * 100, digits = 2)
 
 # run model with interaction between temperature and treatment for rETRmax (is best?)
 #run5_6_photosyn_model <- lmer(formula = rETRmax ~ Treatment * Temperature + (1 | run), data = ulva)
@@ -55,10 +45,10 @@ ulva <- subset(run5_6_photosyn_data, Species == "ul" & RLC.Day == 9)
 
 #ULVA 
 #run model without interaction
-run5_6_photosyn_model_noint <- lmer(formula = rETRmax ~ Treatment + Temperature + (1 | run), data = ulva)
+run5_6_photosyn_model_noint <- lmer(formula = rETRmax ~ Treatment + Temperature + (1 | Run) + (1 | Plant.ID) + (1 | RLC.Order), data = ulva)
 
 #make a histogram and residual plots of the data for ulva
-hist(ulva$rETRmax, main = paste("Ulva lactuca Ek"), col = "olivedrab3", labels = TRUE)
+hist(ulva$rETRmax, main = paste("Ulva lactuca"), col = "olivedrab3", labels = TRUE)
 plot(resid(run5_6_photosyn_model_noint) ~ fitted(run5_6_photosyn_model_noint))
 qqnorm(resid(run5_6_photosyn_model_noint))
 qqline(resid(run5_6_photosyn_model_noint))
@@ -78,13 +68,26 @@ TukeyHSD(ulva_photosyn_model_aov, "Temperature", ordered = FALSE)
 
 plot(allEffects(run5_6_photosyn_model_noint))
 
+#plot a regression between the photosynthetic independent variables of interest and growth rate
+ulva_growth_etr_graph <- ggplot(ulva, aes(x=rETRmax, y=growth_rate)) + geom_point() + 
+  geom_smooth(method = "lm", col = "black") + theme_bw() + 
+  labs(title = "Ulva lactuca rETRmax vs Growth Rate", x = "rETRmax (μmols electrons m-2 s-1)", 
+       y = "growth rate (%)") + stat_regline_equation(label.x = 25, label.y = 165) + stat_cor()
+ulva_growth_etr_graph
+
+ulva_growth_ek_graph <- ggplot(ulva, aes(x=ek.1, y=growth_rate)) + geom_point() + 
+  geom_smooth(method = "lm", col = "black") + theme_bw() + 
+  labs(title = "Ulva lactuca Ek vs Growth Rate", x = "Ek (μmols photons m-2 s-1)", y = "growth rate (%)") + 
+  stat_regline_equation() + stat_cor(label.y = 160)
+ulva_growth_ek_graph
 
 #HYPNEA 
 #run model without interaction
-run5_6_photosyn_model_noint <- lmer(formula = rETRmax ~ Treatment + Temperature + (1 | run), data = hypnea)
+run5_6_photosyn_model_noint <- lmer(formula = rETRmax ~ Treatment + Temperature + (1 | Run) + (1 | Plant.ID) + (1 | RLC.Order), data = hypnea)
+run5_6_photosyn_model_noint <- lmer(formula = ek.1 ~ Treatment + Temperature + (1 | Run) + (1 | Plant.ID) + (1 | RLC.Order), data = hypnea)
 
 #make a histogram and residual plots of the data for hypnea
-hist(hypnea$rETRmax, main = paste("Hypnea musciformis Ek"), col = "maroon", labels = TRUE)
+hist(hypnea$rETRmax, main = paste("Hypnea musciformis"), col = "maroon", labels = TRUE)
 plot(resid(run5_6_photosyn_model_noint) ~ fitted(run5_6_photosyn_model_noint))
 qqnorm(resid(run5_6_photosyn_model_noint))
 qqline(resid(run5_6_photosyn_model_noint))
@@ -104,7 +107,38 @@ TukeyHSD(hypnea_photosyn_model_aov, "Temperature", ordered = FALSE)
 
 plot(allEffects(run5_6_photosyn_model_noint))
 
+#plot a regression between the photosynthetic independent variables of interest and growth rate
+hypnea_growth_etr_graph <- ggplot(hypnea, aes(x=rETRmax, y=growth_rate)) + geom_point() + 
+  geom_smooth(method = "lm", col = "black") + theme_bw() + 
+  labs(title = "Hypnea musciformis rETRmax vs Growth Rate", x = "rETRmax (μmols electrons m-2 s-1)", 
+       y = "growth rate (%)") + stat_regline_equation(label.x = 25, label.y = 165) + stat_cor(label.y = 150)
+hypnea_growth_etr_graph
 
+hypnea_growth_ek_graph <- ggplot(hypnea, aes(x=ek.1, y=growth_rate)) + geom_point() + 
+  geom_smooth(method = "lm", col = "black") + theme_bw() + 
+  labs(title = "Hypnea musciformis Ek vs Growth Rate", x = "Ek (μmols photons m-2 s-1)", y = "growth rate (%)") + 
+  stat_regline_equation(label.y = 155) + stat_cor(label.y = 145)
+hypnea_growth_ek_graph
 
+# do not need this any longer for this dataset
+#get_run_number <- function(d) {
+# returns the run number based on the input date  
+# run = NA
+#if ("2021-09-20" <= d && d <= "2021-09-29") {
+# run <- 1
+#} else if ("2021-09-30" <= d && d <= "2021-10-09") {
+#  run <- 2
+#} else if ("2021-10-11" <= d && d <= "2021-10-29") {
+# run <- 3
+#} else if ("2021-11-01" <= d && d <= "2021-11-12") {
+# run <- 4
+#} else if ("2022-02-11" <= d && d <= "2022-03-01") {
+# run <- 0
+#} else {
+# print("The date is %s not in a recognized range", d)
+#}
+#run
+#}
+#run5_6_photosyn_data$run <- sapply(run5_6_photosyn_data$Date, get_run_number)
 
 
